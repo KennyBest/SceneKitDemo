0425cd5234e416355a774cb9a59df655c4cc3715
lilingjie <kenny@liningdeMacBook-Pro.local>
2017-06-09 01:45:36 +0800

    llj

--- SceneKitDemo/ViewController.swift
+++ SceneKitDemo/ViewController.swift
@@ -7,9 +7,12 @@
 //
 
 import UIKit
+import SceneKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var sceneView: SCNView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +22,23 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        setupScene()
+    }
+    
+    func setupScene() {
+        // 1
+        let scene = SCNScene()
+        
+        // 2
+        let boxGeometry = SCNBox(width: 10.0, height: 10.0, length: 10.0, chamferRadius: 1.0)
+        let boxNode = SCNNode(geometry: boxGeometry)
+        scene.rootNode.addChildNode(boxNode)
+        
+        // 3
+        sceneView.scene = scene
+    }
 }
 
